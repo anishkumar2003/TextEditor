869f819e5ad049fabeb80cca1fd591e6b20bfa4a
Anish Kumar <kumaranish200a@gmail.com>
2024-03-09 14:56:28 +0530

    Routes update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
-import Navbar from './componenets/Navbar';
-import Textform from './componenets/Textform';
+import Navbar from "./componenets/Navbar";
+import Textform from "./componenets/Textform";
 import Alert from "./componenets/Alert";
-import About from './componenets/About';
-import {
-  BrowserRouter as Router,
-  Route,Routes
-} from "react-router-dom";
-// import Switch from 'react-switch';
+import About from "./componenets/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   const [mode, setmode] = useState("light");
   const [alert, setalert] = useState(null);
@@ -34,22 +31,39 @@ function App() {
       ShowAlert("light mode has been activated", "success");
     }
   };
+
   return (
     <>
-    <Router>
-    <Navbar tittle="TextEditor" mode={mode} toglemode={toglemode} />
-    <Alert alert={alert}></Alert>
-    <div className="container">
-    <Routes>
-          <Route path="/about"
-            element={<About />}>
-          </Route>
-          <Route path="/" element={
-            <Textform ShowAlert={ShowAlert} heading="Enter your text to analyze below" m={mode}/>}>
-          </Route>
-        </Routes>
-    </div>
-    </Router>
+      <Router>
+        <Navbar tittle="TextEditor" mode={mode} toglemode={toglemode} />
+        <Alert alert={alert}></Alert>
+        <div className="container">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/"
+              element={
+                <Textform
+                  ShowAlert={ShowAlert}
+                  heading="Enter your text to analyze below"
+                  m={mode}
+                />
+              }
+            />
+            {/* Catch-all route */}
+            <Route
+              path="/*"
+              element={
+                <Textform
+                  ShowAlert={ShowAlert}
+                  heading="Enter your text to analyze below"
+                  m={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
